65101cf8c3aed487f9f3d88fd4904ca3d732578a
bitcoincheck <zmajic0@gmail.com>
2016-07-09 21:16:16 +0200

    Faster

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ for (ii = 1; ii < 6; ii++) {
 TurnOn(pricern[0], 1);
 TurnOn(pricern[1], 2);
 TurnOn(pricern[2], 3);
-TurnOn(pricern[4], 5); }, 1000); },2500);
+TurnOn(pricern[4], 5); }, 100); },250);
 
 function TurnOn(number, group)
 {
